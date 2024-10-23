f29dd17589a75bc7d9d728ec40a69733ac9f9d23
Irfansyah001 <syahirfan545@gmail.com>
2024-10-23 16:12:49 +0700

    menambahkan import library math

--- Calculates_The_Area_Of_a_Circle.py
+++ Calculates_The_Area_Of_a_Circle.py
@@ -1,2 +1,5 @@
 '''2. Use the lambda function to create a function that calculates the area of a circle!
 Input is the length from the center of the circle to the border (jari-jari lingkaran).'''
+
+# Mengimpor library math
+import math
