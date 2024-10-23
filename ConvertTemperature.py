c7be8e7de9a306f2a34c74559e9f8cde9f4676e4
Irfansyah001 <syahirfan545@gmail.com>
2024-10-23 16:08:26 +0700

    Menambahkan Fungsi untuk konversi suhu

--- ConvertTemperature.py
+++ ConvertTemperature.py
@@ -5,3 +5,12 @@ The function accepts two parameters, namely the temperature value and the temper
 # Input unit suhu dan nilai suhu dari pengguna
 unit_temperature = input("Ketikan yang mau di konversi (C/F): ")
 value_temperature = float(input("Masukan nilai suhu yang ingin dikonversi: "))
+
+# Fungsi untuk konversi suhu
+def temperature_funtion(value_temperature, unit_temperature):
+    if unit_temperature == 'C':
+        # Konversi dari Celsius ke Fahrenheit
+        return (value_temperature * 9/5) + 32
+    else:
+        # Konversi dari Fahrenheit ke Celsius
+        return (value_temperature - 32) * 5/9
