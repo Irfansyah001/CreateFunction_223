0fb980b6289338b2b9718b664855e6d57dffcfce
Irfansyah001 <syahirfan545@gmail.com>
2024-10-23 16:05:31 +0700

    menambahkan input unit suhu C/F dan nilai suhu dari user

--- ConvertTemperature.py
+++ ConvertTemperature.py
@@ -1,3 +1,7 @@
 '''1. Create a function that converts temperature from Celsius to Fahrenheit and vice versa.
 The function accepts two parameters, namely the temperature value and the temperature unit
 ('C' for Celsius, 'F' for Fahrenheit).'''
+
+# Input unit suhu dan nilai suhu dari pengguna
+unit_temperature = input("Ketikan yang mau di konversi (C/F): ")
+value_temperature = float(input("Masukan nilai suhu yang ingin dikonversi: "))
