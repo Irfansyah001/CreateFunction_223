b03270bf28540883bbf4d6a6adcfd9ce9d5658dc
Irfansyah001 <syahirfan545@gmail.com>
2024-10-23 15:32:57 +0700

    Soal 1 dan 2

---
+++ Calculates_The_Area_Of_a_Circle.py
@@ -0,0 +1,2 @@
+'''2. Use the lambda function to create a function that calculates the area of a circle!
+Input is the length from the center of the circle to the border (jari-jari lingkaran).'''
---
+++ ConvertTemperature.py
@@ -0,0 +1,3 @@
+'''1. Create a function that converts temperature from Celsius to Fahrenheit and vice versa.
+The function accepts two parameters, namely the temperature value and the temperature unit
+('C' for Celsius, 'F' for Fahrenheit).'''
